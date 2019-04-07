516083928370e3f1b7e0714541ead3171593a862
jamcry <jamcry@hotmail.com>
2019-04-07 20:25:12 +0300

    Completed all functions and tests.

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -8,4 +8,12 @@ class Calculator
     def multiply(*numbers)
         numbers.reduce(:*)
     end
+
+    def subtract(*numbers)
+        numbers.reduce(:-)
+    end
+
+    def divide(*numbers)
+        numbers.reduce(:/)
+    end
 end
--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -15,15 +15,38 @@ RSpec.describe Calculator do
     end
 
     describe "#multiply" do
-       it "multiplies two numbers" do
+       it "returns the multiplication of two numbers" do
             calculator = Calculator.new
             expect(calculator.multiply(3, 5)).to eql(15)
        end
        
-       it "works with multiplication with 0" do
+       it "works if one of the multipliers is zero" do
             calculator = Calculator.new
             expect(calculator.multiply(2, 0)).to eql(0)
        end
     end
 
+    describe "#subtract" do
+        it "returns the difference of two numbers" do
+            calculator = Calculator.new
+            expect(calculator.subtract(5, 2)).to eql(3)
+        end
+        
+        it "returns the subtraction of multiple numbers" do
+            calculator = Calculator.new
+            expect(calculator.subtract(2, 3, 4)).to eql(-5)
+        end
+    end
+
+    describe "#divide" do
+        it "returns the division of two numbers" do
+            calculator = Calculator.new
+            expect(calculator.divide(4, 2)).to eql(2)
+        end
+
+        it "returns the division of multiple numbers in order" do
+            calculator = Calculator.new
+            expect(calculator.divide(8,2,4)).to eql(1)
+        end
+    end
 end
